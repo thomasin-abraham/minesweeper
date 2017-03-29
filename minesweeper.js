ad0fe20eb1334212afc801efd26e68af58cfafe5
thomasin-abraham <thomasinabraham@me.com>
2017-03-29 21:48:10 +1300

    Fix indentation

--- minesweeper.js
+++ minesweeper.js
@@ -23,97 +23,97 @@ function startGame () {
 }
 
 function resetGame (level) {
-  size = level
-  document.getElementsByClassName('board')[0].innerHTML = "";
-  board.cells = []
-  startGame()
+    size = level
+    document.getElementsByClassName('board')[0].innerHTML = "";
+    board.cells = []
+    startGame()
 }
 
 function generateBoard(size) {
-  for(var i=1; i<=size; i++) {
-    for(var j=1; j<=size; j++) {
-    var randomNum = Math.random()
-    var mineCount = 0
-    var generateMine = false;
-    if (randomNum > 0.8) {
-      generateMine = true;
-      mineCount++
+    for(var i=1; i<=size; i++) {
+      for(var j=1; j<=size; j++) {
+        var randomNum = Math.random()
+        var mineCount = 0
+        var generateMine = false;
+        if (randomNum > 0.8) {
+          generateMine = true;
+          mineCount++
+        }
+        var generateCell = {
+          row: i,
+          col: j,
+          isMine: generateMine,
+          isMarked: false,
+          hidden: true,
+          surroundingMines: 0
+        }
+        board.cells.push(generateCell)
+      }
     }
-    var generateCell = {
-      row: i,
-      col: j,
-      isMine: generateMine,
-      isMarked: false,
-      hidden: true,
-      surroundingMines: 0
-    }
-    board.cells.push(generateCell)
-  }
-  }
 }
 
 
 function checkForWin () {
-for(i = 0; i < board.cells.length; i++) {
-if (board.cells[i].isMine && board.cells[i].isMarked) {
-} else if (!board.cells[i].isMine && !board.cells[i].hidden) {
-} else {
-  return
-}
-}
-stopTimer()
-displayMessage("you win!")
+    for(i = 0; i < board.cells.length; i++) {
+      if (board.cells[i].isMine && board.cells[i].isMarked) {
+      } else if (!board.cells[i].isMine && !board.cells[i].hidden) {
+      } else {
+        return
+      }
+    }
+    stopTimer()
+    displayMessage("you win!")
 }
 
 
-
+// Timer code
 
 function startTimer() {
-  timer = setInterval(changeTime,1000)
+    timer = setInterval(changeTime,1000)
 }
 
 function stopTimer() {
-  clearInterval(timer)
+    clearInterval(timer)
 }
 
 function changeTime() {
-  time++;
-  document.getElementById('timer').innerHTML = time;
+    time++;
+    document.getElementById('timer').innerHTML = time;
 }
 
 
-
+// Highlight the level button currently selected
 
 function selectLevel() {
-  var levelButtons = document.getElementsByClassName("gameinfo")[0].children
-  for(var i = 0; i < levelButtons.length; i++) {
-    levelButtons[i].className = "levelButton b" + i
-  }
-  switch (size) {
-    case 3:
-    levelButtons[0].classList.add("onLevel")
-    break;
-    case 4:
-    levelButtons[1].classList.add("onLevel")
-    break;
-    case 5:
-    levelButtons[2].classList.add("onLevel")
-    break;
-    case 6:
-    levelButtons[3].classList.add("onLevel")
-  }
+    var levelButtons = document.getElementsByClassName("gameinfo")[0].children
+    for(var i = 0; i < levelButtons.length; i++) {
+      levelButtons[i].className = "levelButton b" + i
+    }
+    switch (size) {
+      case 3:
+      levelButtons[0].classList.add("onLevel")
+      break;
+      case 4:
+      levelButtons[1].classList.add("onLevel")
+      break;
+      case 5:
+      levelButtons[2].classList.add("onLevel")
+      break;
+      case 6:
+      levelButtons[3].classList.add("onLevel")
+    }
 }
 
 
 
 
 function countSurroundingMines (cell) {
-  var surrounding = lib.getSurroundingCells(cell.row, cell.col);
-  var count = 0;
-  for(var i = 0; i < surrounding.length; i++) {
-    if (surrounding[i].isMine === true) {
-      count++;
+    var surrounding = lib.getSurroundingCells(cell.row, cell.col);
+    var count = 0;
+    for(var i = 0; i < surrounding.length; i++) {
+      if (surrounding[i].isMine === true) {
+        count++;
+      }
     }
-  }
-  cell.surroundingMines = count;
+    cell.surroundingMines = count;
 }
