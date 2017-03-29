f3ad981f50c29d629c524a4044640158a041f92a
thomasin-abraham <thomasinabraham@me.com>
2017-03-29 21:16:24 +1300

    Finish game

--- minesweeper.js
+++ minesweeper.js
@@ -1,36 +1,45 @@
 document.addEventListener('DOMContentLoaded', startGame)
 
-
+// Global Variables
+var size = 4
+var time = 0;
+pauseTime = document.getElementById('pausetime');
+resumeTime = document.getElementById('resumetime');
 var board = {
   cells: []
 }
 
 function startGame () {
-  var size = prompt("How many rows?")
-  if (!isNaN(size) && size < 7) {
-  generateBoard(size)
-  for(var i = 0; i < board.cells.length; i++) {
-    countSurroundingMines(board.cells[i])
-  }
-  lib.initBoard()
-  document.addEventListener("click", checkForWin)
-  document.addEventListener("contextmenu", checkForWin)
-} else if (!isNaN(size) && size > 6) {
-  alert("Board too big (needs to be 6 or under)")
-  startGame()
-} else {
-  alert("Not a number (enter a number)")
-  startGame()
+    generateBoard(size)
+    for(var i = 0; i < board.cells.length; i++) {
+      countSurroundingMines(board.cells[i])
+    }
+    lib.initBoard()
+    displayMessage("minesweeper")
+    clearInterval(timer)
+    time = 0;
+    startTimer()
+    selectLevel()
+    document.addEventListener("click", checkForWin)
+    document.addEventListener("contextmenu", checkForWin)
 }
+
+function resetGame (level) {
+  size = level
+  document.getElementsByClassName('board')[0].innerHTML = "";
+  board.cells = []
+  startGame()
 }
 
 function generateBoard(size) {
   for(var i=1; i<=size; i++) {
     for(var j=1; j<=size; j++) {
     var randomNum = Math.random()
+    var mineCount = 0
     var generateMine = false;
     if (randomNum > 0.8) {
       generateMine = true;
+      mineCount++
     }
     var generateCell = {
       row: i,
@@ -54,8 +63,69 @@ if (board.cells[i].isMine && board.cells[i].isMarked) {
   return
 }
 }
-displayMessage(winMessage)
+stopTimer()
+displayMessage("you win!")
 }
+
+
+
+
+function startTimer() {
+  timer = setInterval(changeTime,1000)
+  pausetime.classList.remove('noshow')
+}
+
+function stopTimer() {
+  console.log(pausetime)
+  clearInterval(timer)
+  if (resumetime.classList.contains("noshow")) {
+  pausetime.classList.add('noshow');
+} else {
+  resumetime.classList.add('noshow');
+}
+}
+
+function changeTime() {
+  time++;
+  document.getElementById('timer').innerHTML = time;
+}
+
+function pauseTimer() {
+  clearInterval(timer)
+  pausetime.classList.add('noshow');
+  resumetime.classList.remove('noshow');
+}
+
+function resumeTimer() {
+  startTimer()
+  resumetime.classList.add('noshow');
+  pausetime.classList.remove('noshow');
+}
+
+
+
+
+function selectLevel() {
+  var levelButtons = document.getElementsByClassName("gameinfo")[0].children
+  for(var i = 0; i < levelButtons.length; i++) {
+    levelButtons[i].className = "levelButton b" + i
+  }
+  switch (size) {
+    case 3:
+    levelButtons[0].classList.add("onLevel")
+    break;
+    case 4:
+    levelButtons[1].classList.add("onLevel")
+    break;
+    case 5:
+    levelButtons[2].classList.add("onLevel")
+    break;
+    case 6:
+    levelButtons[3].classList.add("onLevel")
+  }
+}
+
+
 
 
 function countSurroundingMines (cell) {
